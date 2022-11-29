cf5aa4ab70168bfb43c4bda1b114a298b9ed4a35
BT_3W2_08 <richardh5@nycstudents.net>
2022-11-29 12:14:32 -0500

    go

--- src/quizRunner.java
+++ src/quizRunner.java
@@ -4,6 +4,12 @@ public class quizRunner {
         Scanner qNumb = new Scanner(System.in);
         System.out.println("How many questions do you want to answer out of 4?" + "\n" + " Type R for a random amount of questions");
         String questionNum = qNumb.nextLine();
+        while (!(questionNum.equals("1") || questionNum.equals("2") || questionNum.equals("3") || questionNum.equals("4"))){
+            System.out.println(questionNum);
+            System.out.println("Invalid number, retry");
+            System.out.println("How many questions do you want to answer out of 4?" + "\n" + " Type R for a random amount of questions");
+            questionNum = qNumb.nextLine();
+        }
         quiz q1 = new quiz();
         if (!questionNum.equals("R")) {
             int q = Integer.parseInt(questionNum);
